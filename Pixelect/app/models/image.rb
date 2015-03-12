ee9aa6c48fe5529c0b5090ee802615d5c13ac7c9
Doug <djmacdonald@uchicago.edu>
2015-03-12 11:08:07 -0400

    annotated

--- Pixelect/app/models/image.rb
+++ Pixelect/app/models/image.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id            :integer          not null, primary key
+#  tournament_id :integer
+#  url           :string
+#  wins          :integer
+#  flags         :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Image < ActiveRecord::Base
   belongs_to :tournament
 end
--- Pixelect/app/models/user.rb
+++ Pixelect/app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  password_digest :string
+#  token           :string
+#  email           :string
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
 end
