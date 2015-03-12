3f256562deb95bd200f5995f51e8e4e98a8ad030
JC Gruta <jase.aturg@gmail.com>
2015-03-12 15:50:40 -0400

    Added user request tests.

---
+++ Pixelect/spec/requests/user_request_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'User requests' do
+  before(:each) do
+    @user = User.create!(name: 'Bob', password: 'password', password_confirmation: 'password', email: 'a@a.com')
+    @user2 = User.create!(name: 'Mike', password: 'password2', password_confirmation: 'password2', email: 'm@m.com')
+    @users = User.all
+  end
+
+  describe '#create' do
+    it 'creates a new user' do
+      post "/users",
+      {
+        user: {
+          name: "SamoanThor",
+          email: "samoan_thor@gmail.com",
+          password: '2fast2furious',
+          password_confirmation: '2fast2furious'
+        }
+      }.to_json,
+      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+      expect(response).to be_success
+      expect(response.content_type).to be Mime::JSON
+
+      user = JSON.parse(response.body)
+      expect(user["name"]).to eq "SamoanThor"
+    end
+  end
+
+  describe '#show' do
+    it 'should retrieve a single user by id and return json' do
+      @user = @users.first
+      get "/users/#{@user.id}"
+      expect(response).to be_success
+      user = JSON.parse(response.body)
+      expect(user['name']).to eq @user.name
+    end
+  end
+
+
+end
