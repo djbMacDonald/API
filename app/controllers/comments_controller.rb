de196868e5659e6d441ac9b8895efa40caa64934
Doug <djmacdonald@uchicago.edu>
2015-03-18 10:54:03 -0400

    updating comment will return whole tournament

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
   def update
     @comment = Comment.find(params[:id])
     if @comment.update(edit_comment_params)
-      render json: @comment, status: :ok
+      render json: @comment.tournament, status: :ok
     else
       render json: @comment.errors,
       status: :unprocessable_entity
