c86673cf1ff8f4787ccd80d9e05f5da0b384eb1e
Doug <djmacdonald@uchicago.edu>
2015-03-18 10:52:21 -0400

    added patch to cors

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Pixelect
      config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :destroy, :update, :sign_in, :options]
+        resource '*', :headers => :any, :methods => [:get, :patch, :post, :delete, :destroy, :update, :sign_in, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
